03cc9ce53b988bc217a99635461c217ad0044de2
cbrltpc <cebrail.topac@gmail.com>
2021-08-09 03:05:34 +0200

    true

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -13,8 +13,10 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
+
         tags = "@wip",
-        dryRun = false
+
+        dryRun = true
 
 )
 public class CukesRunner {
